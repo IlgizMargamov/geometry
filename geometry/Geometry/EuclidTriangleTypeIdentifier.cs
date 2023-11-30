c7cdd3f8ced99e54950d9eaa960a1c7c947fa817
GizIOn <34921969+IlgizMargamov@users.noreply.github.com>
2023-11-30 17:50:33 +0500

    fix naming

--- geometry/Geometry/EuclidTriangleTypeIdentifier.cs
+++ geometry/Geometry/EuclidTriangleTypeIdentifier.cs
@@ -6,8 +6,7 @@ namespace Geometry;
 public class EuclidTriangleTypeIdentifier : ITriangleTypeIdentifier
 {
     private const double TOLERANCE = 10e-6;
-
-
+    
     public TriangleType GetTriangleType(double firstSide, double secondSide, double thirdSide)
     {
         if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0)
@@ -22,12 +21,16 @@ public class EuclidTriangleTypeIdentifier : ITriangleTypeIdentifier
         double[] sides = { firstSide, secondSide, thirdSide };
         Array.Sort(sides);
 
-        if (Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) > Math.Pow(sides[2], 2))
+        var firstSidePow = Math.Pow(sides[0], 2);
+        var secondSidePow = Math.Pow(sides[1], 2);
+        var thirdSidePow = Math.Pow(sides[2], 2);
+        
+        if (firstSidePow + secondSidePow > thirdSidePow)
         {
             return TriangleType.ACUTE;
         }
 
-        if (Math.Abs(Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) - Math.Pow(sides[2], 2)) < TOLERANCE)
+        if (Math.Abs(firstSidePow + secondSidePow - thirdSidePow) < TOLERANCE)
         {
             return TriangleType.RIGHT;
         }
